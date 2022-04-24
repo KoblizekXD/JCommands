f8fec00a35c9a451f7d55b9728a3bb6b699f29a9
Koblížek3 <janprokupek@centrum.cz>
2022-04-24 09:27:37 +0200

    Unused imports cleanup

--- src/test/java/TestCommand.java
+++ src/test/java/TestCommand.java
@@ -1,7 +1,5 @@
 import org.jcmds.commands.extendable.legacy.LegacyCommand;
-import org.jcmds.commands.extendable.slash.SlashCommand;
 import org.jcmds.commands.lambda.event.LegacyCommandReceivedEvent;
-import org.jcmds.commands.lambda.event.SlashCommandEvent;
 
 public class TestCommand extends LegacyCommand {
     public TestCommand() {
